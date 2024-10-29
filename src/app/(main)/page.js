3662e6ccfe4932bd4232084ace0be2bea698759d
unknown <grishi349@gmail.com>
2024-10-29 18:23:16 +0530

    404 error page added

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <>YOOOOOO</>;
+  return <>Home Page</>;
 }
---
+++ src/app/not-found.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <section className="flex items-center justify-center h-screen bg-[url('/page/not-found.jpg')] bg-cover ">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-display mb-2">
+          404
+        </h1>
+        <p className="mb-8 text-lg md:text-xl font-normal text-gray-200">
+          The page you requested does not exist.
+        </p>
+        <a
+          href="/"
+          className="items-center justify-center px-5 py-3 text-base font-medium text-center rounded-[24px] bg-accent focus:ring-4 focus:ring-accent hover:bg-accent-secondary"
+        >
+          Go to homepage...
+        </a>
+      </div>
+    </section>
+  );
+}
