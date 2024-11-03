90d22234a6498091badddd585742048cafe2c234
Rishi Raj Gupta <130386735+rishiigupta04@users.noreply.github.com>
2024-11-03 21:03:57 +0530

    Update page.jsx
    changed mobile view layout

--- src/app/(main)/category/[slug]/page.jsx
+++ src/app/(main)/category/[slug]/page.jsx
@@ -28,9 +28,9 @@ export default async function Page({ params, searchParams }) {
           </li>
         </ol>
       </nav>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-y-4   gap-4 mb-6 mt-4">
+      <div className="h-[70vh] grid grid-cols-2 md:grid-cols-3 gap-y-4   gap-4 mb-6 mt-4">
         {games.length === 0 ? (
-          <div className="h-[70vh] text-md text-accent mt-4 ">
+          <div className=" text-md text-accent mt-4 ">
             No games found in this category.
           </div>
         ) : (
